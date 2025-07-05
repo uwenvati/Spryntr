dc3246af852d5160a0c85ede345376a5243814c4
uwenvati <92443061+uwenvati@users.noreply.github.com>
2025-07-05 15:56:58 +0100

    fixed

---
+++ src/app/team/page.tsx
@@ -0,0 +1,7 @@
+export default function TeamPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Team Page</h1>
+    </div>
+  )
+}
